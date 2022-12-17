11726f78c819237470b1a69b5bb2c9ddc521faca
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-17 23:54:45 +0000

    POST request

--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { reset } = require('nodemon');
 
 const app = express();
 
@@ -19,6 +20,23 @@ app.use((req, res, next)=> {
     //flow of execution returns
     const delta = Date.now() - start;
     console.log(`${req.method} ${req.url} ${delta}ms`);
+})
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+    if (!req.body.name){
+       return res.status(400).json({
+            error: 'Missing friend name',
+        });
+    }
+    const newFriend = {
+        name: req.body.name,
+        id: friends.length,
+    };
+    friends.push(newFriend);
+
+    res.json(newFriend)
 })
 
 app.get('/friends', (req, res) => {
