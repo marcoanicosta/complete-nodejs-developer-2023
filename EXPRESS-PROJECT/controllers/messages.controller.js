49295be5e26b8785c55eb6c97a81cab9b98a978a
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-18 20:46:17 +0000

    layout and seperation of concern

--- EXPRESS-PROJECT/controllers/messages.controller.js
+++ EXPRESS-PROJECT/controllers/messages.controller.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 
 function getMessages(req, res) {
-    res.sendFile( path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+    res.render('messages', {
+        title: "Messages to my friends",
+        friend: 'Elon Musk'
+    })
+    //res.sendFile( path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
 }
 
 function postMessages(req, res) {
