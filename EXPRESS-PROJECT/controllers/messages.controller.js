b3c03b045b08d7ce21ee42d2ba55d74b223d4457
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-18 20:38:10 +0000

    templating engines

--- EXPRESS-PROJECT/controllers/messages.controller.js
+++ EXPRESS-PROJECT/controllers/messages.controller.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 
 function getMessages(req, res) {
-    res.sendFile( path.join(__dirname, '..', 'public', 'skimountain.jpg'))
-    //res.send('Updating Messages');
+    res.sendFile( path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
 }
 
 function postMessages(req, res) {
--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -1,9 +1,15 @@
+const { response } = require('express');
 const express = require('express');
+const path = require('path');
 
 const friendsRouter = require('./routes/friends.router')
 const messagesRouter = require('./routes/messages.router')
 
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 const PORT = 3000;
 
@@ -15,7 +21,16 @@ app.use((req, res, next)=> {
     console.log(`${req.method}  ${req.baseUrl}${req.url} ${delta}ms`);
 })
 
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
+
+app.get('/', (req, res)=> {
+    res.render('index', {
+        title: 'My friends are VERY clever',
+        caption: 'lets go skiing!'
+    })
+});
 
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
