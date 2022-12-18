ec99a12578ef670c8e5da71524cab56106800de9
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-18 19:26:59 +0000

    Express Routes

--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -1,33 +1,24 @@
 const express = require('express');
-const { reset } = require('nodemon');
 
-const friendsController = require('./controllers/friends.controller');
-const messagesController = require('./controllers/messages.controller');
+const friendsRouter = require('./routes/friends.router')
+const messagesRouter = require('./routes/messages.router')
 
 const app = express();
 
 const PORT = 3000;
-
-
-
-
 
 app.use((req, res, next)=> {
     const start = Date.now();
     next();
     //flow of execution returns
     const delta = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${delta}ms`);
+    console.log(`${req.method}  ${req.baseUrl}${req.url} ${delta}ms`);
 })
 
 app.use(express.json());
 
-app.post('/friends', friendsController.postFriend);
-app.get('/friends', friendsController.getFriends);
-app.get('/friends/:friendId', friendsController.getFriend);
-
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessages);
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
     console.log(`Listeing on port ${PORT}... 📟`);
