151b6ddf9d76831b004273b2f7923dcf90a4f878
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-18 16:40:54 +0000

    Model View Controller in Express.js

---
+++ EXPRESS-PROJECT/controllers/messages.controller.js
@@ -0,0 +1,15 @@
+
+
+
+function getMessages(req, res) {
+    res.send('Updating Messages');
+}
+
+function postMessages(req, res) {
+    res.send('Updating Messages');
+}
+
+module.exports = {
+    getMessages,
+    postMessages,
+}
--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const { reset } = require('nodemon');
 
+const friendsController = require('./controllers/friends.controller');
+const messagesController = require('./controllers/messages.controller');
+
 const app = express();
 
 const PORT = 3000;
 
 
 
-const friends = [
-    {
-        id: 0,
-        name: 'Isaac Netwon'
-    },
-];
+
 
 app.use((req, res, next)=> {
     const start = Date.now();
@@ -24,41 +22,12 @@ app.use((req, res, next)=> {
 
 app.use(express.json());
 
-app.post('/friends', (req, res) => {
-    if (!req.body.name){
-       return res.status(400).json({
-            error: 'Missing friend name',
-        });
-    }
-    const newFriend = {
-        name: req.body.name,
-        id: friends.length,
-    };
-    friends.push(newFriend);
+app.post('/friends', friendsController.postFriend);
+app.get('/friends', friendsController.getFriends);
+app.get('/friends/:friendId', friendsController.getFriend);
 
-    res.json(newFriend)
-})
-
-app.get('/friends', (req, res) => {
-    res.json(friends)
-});
-
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if (friend) {
-        res.status(200).json(friend);
-    } else {
-        res.status(404).json({
-            error: "Friend does not exist"
-        })
-    }
-});
-
-
-app.post('/messages', (req, res) => {
-    res.send('Updating Messages');
-});
+app.get('/messages', messagesController.getMessages);
+app.post('/messages', messagesController.postMessages);
 
 app.listen(PORT, () => {
     console.log(`Listeing on port ${PORT}... 📟`);
