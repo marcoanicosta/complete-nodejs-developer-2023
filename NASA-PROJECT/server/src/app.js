6dc1efcc60eeed701b445b65e9c56fc460a630c1
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-19 12:33:18 +0000

    serving react.js frontend in production

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -1,4 +1,4 @@
-
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -11,6 +11,10 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
