e7cde79044e828aee09eefdd04164e2412337e42
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-19 11:24:52 +0000

    load data on startup

--- NASA-PROJECT/server/src/server.js
+++ NASA-PROJECT/server/src/server.js
@@ -1,11 +1,17 @@
 const http = require('http');
 const app = require('./app')
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}... 📟`);
-})
+async function startServer(){
+    await loadPlanetsData();
 
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}... 📟`);
+    });
+}
+
+startServer();
