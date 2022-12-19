d3a905f4af17fcb26c5503dd34a92945a110579e
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-19 10:52:29 +0000

    CORS middleware

--- NASA-PROJECT/client/src/hooks/requests.js
+++ NASA-PROJECT/client/src/hooks/requests.js
@@ -1,8 +1,8 @@
-const API_URL = 'https://localhost:8000';
+const API_URL = 'http://localhost:8000';
 
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
-  return response.json();
+  return await response.json();
 }
 
 async function httpGetLaunches() {
