7516cbaf23bdc96612bc92eb61ffcca364cb26ec
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-21 17:00:35 +0000

    clustering in action

--- PERFORMANCE-EXAMPLE/server.js
+++ PERFORMANCE-EXAMPLE/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cluster = require('cluster');
+const os = require('os')
 
 const app = express();
 
@@ -13,12 +15,22 @@ app.get('/', (req, res) => {
     // JSON.stringify({}) => "{}"
     // JSON.parse("{}") => {}
     // [5,1,2,3,4].sort
-    res.send('Performance example');
+    res.send(`Performance example: ${process.pid}`);
 })
 
 app.get('/timer', (req, res) => {
     delay(9000)
-    res.send('Ding ding ding')
+    res.send(`Ding ding ding!  ${process.pid} ⏲️`)
 });
 
-app.listen(3000);
+console.log("Running server.js...📟🚸");
+if (cluster.isMaster) {
+    console.log("Master has been started... 📟");
+    const NUM_WORKERS = os.cpus().length;
+    for (let i = 0; < NUM_WORKERS; i++) {
+        cluster.fork();
+    }
+} else {
+    console.log("Worker process started. 🖲️");
+    app.listen(3000);
+}
