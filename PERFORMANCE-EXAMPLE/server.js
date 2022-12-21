9917731d9883b44180b6344c5fd89cf65ee2a2fd
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-21 17:57:18 +0000

    using pm2 to create clusters

--- PERFORMANCE-EXAMPLE/server.js
+++ PERFORMANCE-EXAMPLE/server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os')
 
 const app = express();
 
@@ -12,9 +10,6 @@ function delay(duration){
 }
 
 app.get('/', (req, res) => {
-    // JSON.stringify({}) => "{}"
-    // JSON.parse("{}") => {}
-    // [5,1,2,3,4].sort
     res.send(`Performance example: ${process.pid}`);
 })
 
@@ -24,14 +19,5 @@ app.get('/timer', (req, res) => {
 });
 
 console.log("Running server.js...📟🚸");
-if (cluster.isMaster) {
-    console.log("Master has been started... 📟");
-    const NUM_WORKERS = os.cpus().length;
-    for (let i = 0; i < NUM_WORKERS; i++) {
-        cluster.fork();
-        console.log(i);
-    }
-} else {
-    console.log("Worker process started. 🖲️");
-    app.listen(3000);
-}
+console.log("Worker process started. 🖲️");
+app.listen(3000);
