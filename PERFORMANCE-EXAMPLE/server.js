76c28520e36421b23efb5e80aed5b405d0b6c464
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-21 16:14:39 +0000

    node server optimisation start/test

---
+++ PERFORMANCE-EXAMPLE/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+function delay(duration){
+    const startTime = Date.now();
+    while(Date.now() - startTime < duration) {
+        //event loop 
+    }
+}
+
+app.get('/', (req, res) => {
+    // JSON.stringify({}) => "{}"
+    // JSON.parse("{}") => {}
+    // [5,1,2,3,4].sort
+    res.send('Performance example');
+})
+
+app.get('/timer', (req, res) => {
+    delay(9000)
+    res.send('Ding ding ding')
+});
+
+app.listen(3000);
