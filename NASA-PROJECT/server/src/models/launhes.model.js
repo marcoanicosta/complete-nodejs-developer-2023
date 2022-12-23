a8b1290fa182cebaf8911e448f30f326db591d64
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-23 19:07:21 +0000

    saving launches

--- NASA-PROJECT/server/src/models/launhes.model.js
+++ NASA-PROJECT/server/src/models/launhes.model.js
@@ -1,4 +1,4 @@
-//const launches = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
 const launches = new Map();
 
 let latestFlightNumber = 100;
@@ -14,7 +14,7 @@ const launch = {
     success: true,
 }
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 
 function existsLaunchWithId(launchId) {
@@ -23,6 +23,14 @@ function existsLaunchWithId(launchId) {
 
 function getAllLaunches(){
     return Array.from(launches.values());
+}
+
+async function saveLaunch(launch) {
+    await launchesDatabase.updateOne({
+        flightNumber: launch.flightNumber,
+    }, launch, {
+        upsert: true,
+    });
 }
 
 function addNewLaunch(launch) {
