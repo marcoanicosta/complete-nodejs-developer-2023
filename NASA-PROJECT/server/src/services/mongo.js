65823a4bb7927f0abf5bef36ade58b9aefd178b8
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-26 21:30:17 +0000

    managing secrets with dotenv

--- NASA-PROJECT/server/src/services/mongo.js
+++ NASA-PROJECT/server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:64nzHJO0OXt2psPV@cluster0.gcdrucm.mongodb.net/nasa?retryWrites=true&w=majority'
-
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
