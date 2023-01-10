ea7c118ce77d3e441367bd2ee92f572f776de084
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-01-10 00:34:02 +0000

    modulising graphQL Schemas II

---
+++ GRAPHQL-EXAMPLE/orders/orders.model.js
@@ -0,0 +1,18 @@
+module.exports = [
+    [
+        {
+            date: '2005-05-05',
+            subtotal: 90.22,
+            items: [
+                {
+                    product: {
+                        id: 'redshoe',
+                        description: 'Olde Red Shoe',
+                        price: 45.11,
+                },
+                quantity: 2,
+                }  
+            ]
+        }
+    ]
+]
--- GRAPHQL-EXAMPLE/server.js
+++ GRAPHQL-EXAMPLE/server.js
@@ -1,62 +1,24 @@
+const path = require('path');
 const express = require('express');
-const { buildSchema } = require('graphql');
-const { graphqlHTTP } = require('express-graphql')
 
-const schema = buildSchema(`	
-type Query {	
-  products: [Product]	
-  orders: [Order]	
-}	
-type Product {	
-  id: ID!	
-  description: String!	
-  reviews: [Review]	
-  price: Float!	
-}	
-type Review {	
-  rating: Int!	
-  comment: String	
-}	
-type Order {	
-  date: String!	
-  subtotal: Float!	
-  items: [OrderItem]	
-}	
-type OrderItem {	
-  product: Product!	
-  quantity: Int!	
-}	
-`);	
-const root = {	
-products: [	
-  {	
-    id: 'redshoe',	
-    description: 'Red Shoe',	
-    price: 42.12,	
-  },	
-  {	
-    id: 'bluejean',	
-    description: 'Blue Jeans',	
-    price: 55.55,	
-  }	
-],	
-orders: [	
-  {	
-    date: '2005-05-05',	
-    subtotal: 90.22,	
-    items: [ 	
-      {	
-        product: {	
-          id: 'redshoe',	
-          description: 'Old Red Shoe',	
-          price: 45.11,	
-        },	
-        quantity: 2,	
-      }	
-    ]	
-  }	
-]	
-};
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { graphqlHTTP } = require('express-graphql');
+
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+
+
+const typesArray = loadFilesSync(path.join(__dirname,'**/*.graphql'));
+//const { makeExecutableSchema } = loadFilesSync( '@graphql-tools/schema'));
+
+
+const schema = makeExecutableSchema ({
+    typeDefs: typesArray
+});
+
+const root = {
+    products: require('./products/products.model'),
+    orders: require('./orders/orders.model'),
+}
 
 const app = express();
 
