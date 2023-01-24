9491edbcfb2b85c6763298ca5cb2b05eef63c3cb
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-01-24 22:39:10 +0000

    first deno app

---
+++ DENO-PROJECT/deno.js
@@ -0,0 +1,19 @@
+const food = Deno.args[0];
+Deno.
+
+if(food == 'love') {
+    console.log('🦕. Deno is born!');
+    } else {
+        console.log('🥚.. this egg needs some love');
+    }
+
+
+
+    // const food = 'love'
+
+    // if(food == 'love') {
+    //     console.log('🦕. Deno is born!');
+    //     } else {
+    //         console.log('🥚.. this egg needs some love');
+    //     }
+    
